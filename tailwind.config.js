59d61e039feb02f7ba30cb7e619bc1ff25518e1a
rinchjo <83864991+rinchjo@users.noreply.github.com>
2022-01-07 01:10:11 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        newImg1:
+          "url(http://amjiltacademy.com/wp-content/uploads/2021/10/KPS.jpg)",
+        newImg2:
+          "url(http://amjiltacademy.com/wp-content/uploads/2021/10/design.jpg)",
+        newImg3:
+          "url(http://amjiltacademy.com/wp-content/uploads/2021/10/Python.jpg)",
+        newImg4:
+          "url(http://amjiltacademy.com/wp-content/uploads/2021/10/ХАБЭА-1.jpg)",
+        bImage:
+          "url(http://amjiltacademy.com/wp-content/uploads/2021/10/dark8.jpg)",
+      },
+    },
+  },
+  plugins: [],
+};
